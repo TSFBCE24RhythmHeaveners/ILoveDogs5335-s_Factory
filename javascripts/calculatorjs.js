36a155e466caf89f25b3486b883a07e9a3d04caf
MTPASLE763 On Github <zhygersamueloliquiano@gmail.com>
2026-05-17 20:21:40 +0400

    Make it a function for a button.

--- javascripts/calculatorjs.js
+++ javascripts/calculatorjs.js
@@ -1,6 +1,7 @@
 var number_1, number_2;
 
-
-number_1 = Number(window.prompt('Enter the first number.'));
-number_2 = Number(window.prompt('Enter the second number.'));
-window.alert(['Addition result: ' + String(number_1 + number_2),'Subtraction result: ' + String(number_1 - number_2),'Multiplication result: ' + String(number_1 * number_2),'Division result: ' + String(number_1 / number_2),'Exponentation result: ' + String(Math.pow(number_1, number_2)),'Root result: ' + String(Math.pow(number_1, 1 / number_2))].join(''));
+function calculator() {
+  number_1 = Number(window.prompt('Enter the first number.'));
+  number_2 = Number(window.prompt('Enter the second number.'));
+  window.alert(['Addition result: ' + String(number_1 + number_2),'. Subtraction result: ' + String(number_1 - number_2),'. Multiplication result: ' + String(number_1 * number_2),'. Division result: ' + String(number_1 / number_2),'. Exponentation result: ' + String(Math.pow(number_1, number_2)),['. Root result: ',Math.pow(number_1, 1 / number_2),'.'].join('')].join(''));
+}
