b449cfaa41f6370a279f7e997b1ad00507e7aac3
MTPASLE763 On Github <zhygersamueloliquiano@gmail.com>
2026-05-17 19:42:07 +0400

    e
    Delete that one Personal Home Page: Hypertext Processor file.

--- personalhps/calculator.php
+++
@@ -1,3 +0,0 @@
-$number_1 = floatval(readline('Enter the first number.'));
-$number_2 = floatval(readline('Enter the second number.'));
-print((('The addition is ' . (($number_1 + $number_2) . '. ')) . (('The subtraction is ' . (($number_1 - $number_2) . '. ')) . ('The multiplication is ' . ($number_1 * $number_2 . '. ')))) . (('The division is ' . ($number_1 / $number_2 . '. ')) . (('The exponentation is ' . ($number_1 ** $number_2 . '. ')) . ('The rooting is ' . ($number_1 ** (1 / $number_2) . '.')))));
